0938be12bddf86b115ac887c520eee302a6fc479
Divyansh Moonat <divyanshmoonat@outlook.com>
2023-08-31 23:29:38 +0530

    FS and Events module

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const os = require('node:os');
 const fs = require("node:fs");
+const events = require("node:events");
+
 
 // import axios from 'axios'; // ESM
 // const axios = require("axios"); // CJS
@@ -54,9 +56,7 @@ const writeFile = (fileContent) => {
         }
     });
 };
-// writeFile(`Hi there
-// this is a multi line
-// data inserted into file`);
+// writeFile(`Hello from fs module`);
 
 
 /**
@@ -70,7 +70,76 @@ const readFile = () => {
             return;
         }
         console.log("FILE DATA", fileData.toString());
+        // return fileData.toString();
     })
 };
 
-readFile();
+// readFile();
+
+const updateFile = () => {
+    /**
+     * 1. Read the file
+     * 2. Modify the contents
+     * 3. Write the file
+     */
+
+    // const fileData = fs.readFileSync("logs.txt");
+
+    // let contents = fileData.toString();
+    // contents = contents + "\nFILE IS EDITED USING FS MODULE";
+
+    // fs.writeFileSync("logs.txt", contents);
+
+    // console.log("FILE EDITED SUCCESSFULLY");
+
+    fs.appendFileSync("logs.txt", "\nThis is new line with new method");
+};
+
+// updateFile();
+
+const deleteFile = () => {
+    fs.unlinkSync("logs.txt");
+};
+
+// deleteFile();
+
+
+let colorIndex = 0;
+
+const writeLogs = (color) => {
+    const timeStamp = new Date();
+    const logStr = timeStamp + " Signal changed to " + color + "\n";
+    // 31/08/2023 21:41:00 Signal changed to Green
+    fs.appendFileSync("signal.log", logStr);
+};
+
+// setInterval(() => {
+//     writeLogs();
+//     if (colorIndex === 2) {
+//         colorIndex = 0;
+//     } else {
+//         colorIndex++;
+//     }
+// }, 5000);
+
+/**
+ * 1. Emit the event
+ * 2. Listen the event
+ */
+
+const myEmitter = new events.EventEmitter();
+
+const SIGNAL_CHANGE = 'SIGNAL_CHANGE';
+
+myEmitter.on(SIGNAL_CHANGE, writeLogs);
+
+const colors = ["Green", "Yellow", "Red"];
+
+// setInterval(() => {
+//     myEmitter.emit(SIGNAL_CHANGE, colors[colorIndex]);
+//     if (colorIndex === 2) {
+//         colorIndex = 0;
+//     } else {
+//         colorIndex++;
+//     }
+// }, 5_000);
