a9c6f4be60fbc6cf7e5e1c368d02f7231d7a0ce1
Divyansh Moonat <divyanshmoonat@outlook.com>
2023-09-07 23:12:56 +0530

    APIS

--- index.js
+++ index.js
@@ -45,7 +45,9 @@ const products = [
 
 ];
 
-app.get("/product-list", (req, res) => {
+const apiVersion = 'v1';
+
+app.get(`/api/${apiVersion}/product-list`, (req, res) => {
     res.json({
         success: true,
         results: products
@@ -53,13 +55,13 @@ app.get("/product-list", (req, res) => {
     // res.send("Product list API");
 });
 
-app.post("/product-list", (req, res) => {
+app.post(`/api/${apiVersion}/product-list`, (req, res) => {
     res.json({
         success: true
     })
 });
 
-app.get("/product-details/:productId", (req, res) => {
+app.get(`/api/${apiVersion}/product-details/:productId`, (req, res) => {
     const productId = req.params.productId;
     const product = products.find(p => p.id == productId);
 
@@ -68,6 +70,52 @@ app.get("/product-details/:productId", (req, res) => {
         result: product
     });
 });
+
+app.put(`/api/${apiVersion}/replace-product`, (req, res) => {
+    //ToDo : put your logic here
+    res.json({
+        success: true
+    });
+});
+
+app.delete(`/api/${apiVersion}/product/delete`, (req, res) => {
+    res.status(404).json({
+        success: true
+    })
+});
+
+// /user/login
+//     / user / register
+//     / user / edit
+
+app.post(`/api/${apiVersion}/user/register`, (req, res) => {
+    res.json({
+        success: true,
+        message: "Dummy Registration API"
+    })
+});
+
+app.post(`/api/${apiVersion}/user/login`, (req, res) => {
+    res.json({
+        success: true,
+        message: "Dummy Login API"
+    })
+});
+
+app.patch(`/api/${apiVersion}/user/edit`, (req, res) => {
+    res.json({
+        success: true
+    })
+});
+
+app.get(`/api/${apiVersion}/cart`, (req, res) => {
+    res.json({
+        success: true,
+        message: "Dummy cart API"
+    })
+});
+
+// console.log(process.env.API_KEY);
 
 const portNo = 8081;
 // server.listen(8081, callbackFn);
